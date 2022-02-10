8117b1cb8fc4dcd87f4e07a578ed634cd326103e
Alexis Ortega (__Anniee__) <oretagalexis@gmail.com>
2022-02-10 16:50:01 -0600

    Nft card example

---
+++ main.js
@@ -0,0 +1,8 @@
+const view = document.getElementById("view")
+
+view.addEventListener("mouseover",()=> { 
+    view.style.opacity = "1"
+})
+view.addEventListener("mouseout",()=> { 
+    view.style.opacity = "0"
+})

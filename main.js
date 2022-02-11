8b91f5ae914945dbf84c6abfce4f5c61a1a8c66f
Alexis Ortega (__Anniee__) <oretagalexis@gmail.com>
2022-02-11 03:24:29 -0600

    Final version

--- main.js
+++ main.js
@@ -6,3 +6,45 @@ view.addEventListener("mouseover",()=> {
 view.addEventListener("mouseout",()=> { 
     view.style.opacity = "0"
 })
+
+//model for the ntf view
+//REQUESTED BY JEAN
+
+const modal = document.createElement("DIV");
+const fragment = document.createDocumentFragment()
+const imageCont = document.createElement("DIV")
+const closePreview = document.createElement("DIV")
+
+modal.appendChild(closePreview)
+modal.appendChild(imageCont)
+fragment.appendChild(modal)
+document.body.appendChild(fragment)
+
+modal.classList.add("modal-image")
+imageCont.classList.add("image-cont")
+closePreview.classList.add("close")
+
+
+
+view.addEventListener( "click", ()=> {
+    modal.classList.add("closed-modal")
+    
+    if( modal.classList.contains("closed-modal")) { 
+        modal.style.display = "grid";
+        modal.style.width = "100%"
+        modal.style.height = "100%"
+        
+        modal.classList.remove("closed-modal")
+        modal.classList.add("open-modal")
+    }
+})
+
+closePreview.addEventListener( "click", ()=> { 
+    if( modal.classList.contains("open-modal")) { 
+        modal.style.width = "0%"
+        modal.style.height = "0%"
+        
+        modal.classList.remove("open-modal")
+        modal.classList.add("close-modal")
+    }
+})
